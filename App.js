70b6f51b0e9015bf99506f71250137f4d6b14129
Woopsoops <parviainen.joni@gmail.com>
2022-01-24 22:32:31 +0200

    laskintehtävä

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Button, Text, TextInput, } from 'react-native';
 
 export default function App() {
+  const [number1, setNumber1] = useState('');
+  const [number2, setNumber2] = useState('');
+  const [total, setTotal] = useState('');
+
+function calculateAdd() {
+  setTotal(parseInt(number1) + parseInt(number2));
+}
+function calculateSubtr() {
+  setTotal(parseInt(number1) - parseInt(number2));
+}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+  
+  <Text>Result: {total} </Text>
+    <TextInput keyboardType="numeric" style={styles.input} value={number1} onChangeText={number1 => setNumber1(number1)}/>
+    <TextInput keyboardType="numeric" style={styles.input} value={number2} onChangeText={number2 => setNumber2(number2)} />
+  
       <StatusBar style="auto" />
+
+      <View style={{ flexDirection: "row" }}>
+      <Button
+        title="+"
+        onPress={calculateAdd}
+      />
+      <Button
+        title="-"
+        onPress={calculateSubtr}
+      />
+      </View>
     </View>
   );
 }
@@ -17,4 +44,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input : {
+    width:200  , 
+    borderColor: 'pink', 
+    borderWidth: 3
+  }
 });
